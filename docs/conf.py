d303ed06542ed7c26cf2ef7654ee384c40672934
Luca De Vitis <luca@monkeython.com>
2014-09-11 15:21:49 +0200

    Read the Docs build fix.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 import loremipsum
 
