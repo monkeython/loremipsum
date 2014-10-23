d822ded1c343f30b7f4ab92980b1ff91732d63f6
Luca De Vitis <luca@monkeython.com>
2014-10-23 09:54:27 +0200

    Moved into package

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from tests import test_functions
-from tests import test_generator
-
-suite = unittest.TestSuite()
-suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_generator))
-suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_functions))
