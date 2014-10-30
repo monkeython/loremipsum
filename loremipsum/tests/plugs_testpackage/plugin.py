f081906482bf080363dd494a6ab0ca6ed63b49f5
Luca De Vitis <luca@monkeython.com>
2014-10-30 11:19:12 +0100

    Put useless module function into docstring

--- loremipsum/tests/plugs_testpackage/plugin.py
+++ loremipsum/tests/plugs_testpackage/plugin.py
@@ -1,14 +1,13 @@
-"""Test plugin."""
+"""Test plugin.
 
 
-def load(*args, **kwargs):
-    pass
+def load(*args, **kwargs): pass
 
 
-def dump(*args, **kwargs):
-    pass
+def dump(*args, **kwargs): pass
 
 
 def plugin():
-    import sys
     return (__name__.split('.')[-1], sys.modules.get(__name__))
+
+"""
