b16e319cb891dcd7ae18b9c760699702288cc1cd
Luca De Vitis <luca@monkeython.com>
2014-10-30 11:21:55 +0100

    Do not import data and package scheme: exparimental

--- loremipsum/serialization/schemes/__init__.py
+++ loremipsum/serialization/schemes/__init__.py
@@ -1,5 +1,3 @@
-from loremipsum.serialization.schemes import data
 from loremipsum.serialization.schemes import file_
-from loremipsum.serialization.schemes import package
 
-__all__ = ['data', 'file_', 'package']
+__all__ = ['file_']
