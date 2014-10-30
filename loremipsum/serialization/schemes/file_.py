b9fbd1e0dadb691c905390d2417e9fcd15cdd90a
Luca De Vitis <luca@monkeython.com>
2014-10-30 11:45:59 +0100

    Python 2 to 3 fixing

--- loremipsum/serialization/schemes/file_.py
+++ loremipsum/serialization/schemes/file_.py
@@ -9,47 +9,58 @@ from loremipsum.serialization import content_types
 
 def load(class_, url, **args):
     """Loads the sample from a file URL."""
-    if url.netloc and url.netloc != 'localhost':
-        raise NotImplementedError('Cannot load from a location other than'
-                                  ' localhot: {}'.format(url.netloc))
     if os.path.isdir(url.path):
-        args = {
-            'sample': None,
+        files = {
+            'text': None,
             'lexicon': None,
             'word_delimiters': None,
             'sentence_delimiters': None}
-        for arg_name in args:
-            with open(os.path.join(url.path, arg_name + '.txt'), 'rb') as arg:
-                args[arg_name] = arg.read().decode('utf-8')
-        return class_(**args)
+        for filename in files:
+            with open(os.path.join(url.path, filename + '.txt'), 'rb') as txt:
+                files[filename] = txt.read().decode('UTF-8')
+        return class_(**files)
     else:
         with open(url.path, 'rb') as file_:
             content = file_.read()
-        mimetype, enconding = mimetypes.guess_type(url.path)
-        if enconding:
-            content = content_encodings.get(enconding).decode(content)
-        return class_(**content_types.get(mimetype).parse(content))
+        mimetype, encoding = mimetypes.guess_type(url.path)
+        content_encoding = args.get('content_encoding', encoding)
+        content_type = args.get('content_type', mimetype)
+        if content_encoding:
+            content = content_encodings.get(content_encoding).decode(content)
+        frozen = content_types.get(content_type).parse(content)
+        return class_(**frozen)
 
 
 def dump(sample, url, **args):
     """Dunps the sample into a file URL."""
-    if url.netloc and url.netloc != 'localhost':
-        raise NotImplementedError('Cannot dump to a location other than'
-                                  ' localhot: {}'.format(url.netloc))
     prefix, extension = os.path.splitext(url.path)
     if not extension:
+        if not os.path.exists(url.path):
+            os.mkdir(url.path)
         files = {
-            'sample': sample['text'],
+            'text': sample['text'],
             'lexicon': sample['lexicon'],
             'word_delimiters': sample['word_delimiters'],
             'sentence_delimiters': sample['sentence_delimiters']}
         for filename in files:
             with open(os.path.join(prefix, filename + '.txt'), 'wb') as file_:
-                file_.write(files.get(filename))
+                file_.write(files.get(filename).encode('UTF-8'))
     else:
-        mimetype, enconding = mimetypes.guess_type(url.path)
-        content = content_types.get(mimetype).format(sample.frozen())
-        if enconding:
-            content = content_encodings.get(enconding).encode(content)
+        mimetype, encoding = mimetypes.guess_type(url.path)
+        content_encoding = args.get('content_encoding', encoding)
+        content_type = args.get('content_type', mimetype)
+        content = content_types.get(content_type).format(sample.frozen())
+        if content_encoding:
+            content = content_encodings.get(content_encoding).encode(content)
         with open(url.path, 'wb') as file_:
             file_.write(content)
+
+
+def remove(url, **args):
+    """Remove the sample from a file URL."""
+    if os.path.isdir(url.path):
+        for filename in os.listdir(url.path):
+            os.remove(os.path.join(url.path, filename))
+        os.rmdir(url.path)
+    else:
+        os.remove(url.path)
