e8aaa6c90f20191ef4491b91c50002ef5d8e19db
Luca De Vitis <luca@monkeython.com>
2014-10-30 11:18:25 +0100

    Added serialization TestCase(s)

--- loremipsum/tests/testcases.py
+++ loremipsum/tests/testcases.py
@@ -4,12 +4,6 @@ import unittest
 import loremipsum
 
 
-try:
-    unicode_str = __builtins__.get('unicode')
-except AttributeError:
-    unicode_str = str
-
-
 class TestPackagesPlugs(unittest.TestCase):
     """Packages plugs TestCase."""
 
@@ -41,3 +35,50 @@ class TestPackagesPlugs(unittest.TestCase):
         classes = (types.ModuleType, loremipsum.generator.Sample)
         self.assertIs(self._package.DEFAULT, self._package.get(self._DEFAULT))
         self.assertIsInstance(self._package.DEFAULT, classes)
+
+
+class TestSerializationScheme(unittest.TestCase):
+
+    def test_dump_load(self):
+        """Test dump/load functionss of a serialization scheme module."""
+        for url, args in self._urls.items():
+            self._dump(url, **args)
+            self._load(url, **args)
+            self._remove(url, **args)
+
+    def _dump(self, url, **args):
+        """Apply the Sample.dump method."""
+        self._sample.dump(url, **args)
+
+    def _load(self, url, **args):
+        """Apply the Sample.load classmethod."""
+        original = self._sample
+        self.assertEqual(original, self._sample.load(url, **args))
+
+    def _remove(self, url, **args):
+        """Apply the Sample.remove method."""
+        self._sample.remove(url)
+        with self.assertRaises(self._CannotLoadRemoved):
+            self._sample.load(url, **args)
+        with self.assertRaises(self._CannotRemoveAgain):
+            self._sample.remove(url)
+
+
+class TestSerializationContentType(unittest.TestCase):
+
+    def test_format_parse(self):
+        """Test format/parse functions of a content_types module."""
+        frozen = loremipsum.samples.DEFAULT.frozen()
+        formatted = self._type.format(frozen)
+        parsed = self._type.parse(formatted)
+        sample = loremipsum.generator.Sample(**parsed)
+        self.assertEqual(loremipsum.samples.DEFAULT, sample)
+
+
+class TestSerializationContentEncoding(unittest.TestCase):
+
+    def test_encode_decode(self):
+        """Test encode/decode functions of a content_encodings module."""
+        frozen = (''.join(loremipsum.samples.DEFAULT.row())).encode('UTF-8')
+        sample = self._encoding.decode(self._encoding.encode(frozen))
+        self.assertEqual(frozen, sample)
