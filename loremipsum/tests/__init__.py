2b06493e65ea1d3cc63ee107b69b0667ebea726e
Luca De Vitis <luca@monkeython.com>
2014-10-30 11:17:41 +0100

    Flake8 complaints fixing.

--- loremipsum/tests/__init__.py
+++ loremipsum/tests/__init__.py
@@ -4,7 +4,7 @@ from loremipsum.tests import plugs_testpackage
 from loremipsum.tests import test_generator
 from loremipsum.tests import test_loremipsum
 from loremipsum.tests import test_plugs
-# from loremipsum.tests import test_serialization
+from loremipsum.tests import test_serialization
 
 __all__ = [
     'plugs_testpackage',
@@ -13,7 +13,8 @@ __all__ = [
     'test_plugs']
 
 suite = unittest.TestSuite()
-suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_generator))
-suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_loremipsum))
-suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_plugs))
-# suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_serialization))
+loader = unittest.defaultTestLoader
+suite.addTest(loader.loadTestsFromModule(test_generator))
+suite.addTest(loader.loadTestsFromModule(test_loremipsum))
+suite.addTest(loader.loadTestsFromModule(test_plugs))
+suite.addTest(loader.loadTestsFromModule(test_serialization))
